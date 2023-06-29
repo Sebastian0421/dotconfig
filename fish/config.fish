285a5c166e3377c8b42ce3aa9cf3323e75fe3984
Sebastian0421 <zhutiantian0421@gmail.com>
2023-06-29 20:18:46 +0800

    auto get display

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,8 @@
 set -g -x RANGER_LOAD_DEFAULT_RC FALSE
 set -g -x PATH /usr/local/matlab/R2020a/bin/ $PATH
-#set -g -x DISPLAY 192.168.0.101:0
-#set -g -x DISPLAY 10.192.214.67:0
+#set -g -x DISPLAY  10.192.130.199:0
+set -g -x DISPLAY $(ifconfig | grep inet | awk '{print $2}' | head -n 1 | awk '{print $0":0"}')
+set -g -x XDG_RUNTIME_DIR /tmp/runtime-tt
 set -g fish_greeting
 set -g -x PATH /usr/local/matlab/R2020a/bin/glnxa64/ $PATH
 
